916f217ec39f9efc4eada87f99f32fd0050b43ad
Karim B <mattiakarim@live.it>
2019-07-26 09:25:42 +0200

    Chore: installed dependencies and created model routes, also added .env file with secret for the jwt

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('./model-route');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,8 +10,27 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register (req, res) {
   // implement user registration
+  try {
+    let { username, password } = req.body;
+
+    if(username && password) {
+        password = bcrypt.hashSync(password, 12);
+
+        const user = await db.registerUser({ username, password });
+
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({
+            message: 'Missing credentials'
+        });
+    }
+} catch(error) {
+    res.status(500).json({
+        message: 'Server error while registering user'
+    });
+}
 }
 
 function login(req, res) {
@@ -18,7 +39,9 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { 
+      accept: 'application/json' 
+    }
   };
 
   axios
@@ -27,6 +50,8 @@ function getJokes(req, res) {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ 
+        message: 'Error Fetching Jokes', error: err 
+      });
     });
 }
